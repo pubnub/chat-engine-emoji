05e3db605c5d459de5f950f2462b236db12ecc47
Ian Jennings <ian@meetjennings.com>
2017-07-28 18:10:35 -0500

    add jsdoc documentation

--- src/plugin.js
+++ src/plugin.js
@@ -1,8 +1,35 @@
+/**
+ *
+* @module chat-engine-emoji
+* @description  Parses emoji in ```payload.data.text```.
+*/
+
 const emojis = require('./emoji.js');
 const dotty = require("dotty");
 
 // this is an example of middleware used in our test.js
 // adds some text to message before it's sent and when it's received
+
+/**
+* @function
+* @ceplugin
+* @requires {@link ChatEngine}
+* @param {Object} config The config object
+* @param {Function} config.title The title of the desktop notification
+* @example
+*
+*
+* const emoji = require('./src/plugin.js');
+* chat = new OCF.Chat('emoji-chat');
+* chat.plugin(emoji());
+* chat.on('message', (payload) => {
+*     // payload.data.text == '<img class="emoji" title=":pizza:" alt="pizza" src="http://www.webpagefx.com/tools/emoji-cheat-sheet/graphics/emojis/pizza.png" height="16" />';
+* });
+*
+* chat.emit('message', {
+*     text: 'I want :pizza:'
+* });
+*/
 module.exports = (config) => {
 
     // regular expression to find emoji strings
@@ -45,6 +72,12 @@ module.exports = (config) => {
 
     });
 
+    /**
+    * Turns```:smile:``` into ```<img src="/smile.png" />```
+    * @listens message
+    * @listens $history.message
+    * @ceextends Chat
+    */
     let parseEmoji = function(payload, next) {
 
         let message = dotty.get(payload, config.prop);
@@ -65,9 +98,27 @@ module.exports = (config) => {
 
     // these are new methods that will be added to the extended class
     class extension {
+
+        /**
+         * Renders emoji given ```:smile:``` as input.
+         * @method render
+         * @ceextends Chat
+         * @param  {String} string The emoji text to turn into an icon
+         * @param  {String} url Root url to look for emoji images
+         * @param  {Int} height Height of the emoji icons
+         * @returns {String} Returns the IMG HTML for this emoji
+         */
         render(string, url, height) {
             return emoji(string, url, height);
         }
+
+        /**
+         * Finds partial string matches of emoji text by searching emoji db.
+         * @method search
+         * @ceextends Chat
+         * @param  {Strings} query The partial text to search for
+         * @returns {Array} An array of matching emoji strings. Call the render function to display these.
+         */
         search(query) {
 
             var results = [];
